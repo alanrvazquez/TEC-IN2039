2d71aeca82f70cc0359af818a9757a99b2e4e2e7
Alan Vazquez <alanrvazquez@gmail.com>
2024-02-12 15:16:09 -0600

    Create scratch.R

---
+++ Session_1_Introduction/scratch.R
@@ -0,0 +1,44 @@
+
+## ***Principio 2*****: *Transforma los datos en información***
+
+Tu gráfica debe de usar los datos para transmitir el mensaje o contestar la pregunta. Es decir, debe de transformar los datos en información.
+
+::: center
+::: columns
+::: {.column width="33%"}
+![](images/clipboard-4189732703.png)
+:::
+  
+  ::: {.column width="33%"}
+![](images/clipboard-1399795317.png)
+:::
+  
+  ::: {.column width="33%"}
+![](images/clipboard-2893827168.png)
+:::
+  :::
+  :::
+  
+## *Principio 3: Enriquece con información adicional*
+
+Enriquece tu gráfica con símbolos de color y texto para transmitir información adicional.
+
+::: center
+::: columns
+::: {.column width="50%"}
+![](images/clipboard-2010705704.png)
+:::
+  
+  ::: {.column width="50%"}
+![](images/clipboard-2746624499.png)
+:::
+  :::
+  :::
+  
+  ::: notes
+No te limites a cosas simples. Enriquece tu gráfica con símbolos de color para transmitir información adicional. Si es posible, agrega contexto con marcadores y etiquetas de referencia.
+
+También, agrega una leyenda a la grafica que describa las características importantes y resuma sus conclusiones.
+:::
+  
+  ## 
